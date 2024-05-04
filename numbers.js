58bc6045ab83e4b199aec6f3b7e3020b8656d631
Paweł Firlej <polf@polf.pl>
2024-05-04 23:21:14 +0200

    Update numbers.js
    dodano wyświetlenie wszystkich liczb w tablicy przed pętlą iteracji

--- numbers.js
+++ numbers.js
@@ -1,8 +1,8 @@
 const numbers = [13, 5, 7, 55, 34, 23, 1, 3];
 const treshold = 5;
-
+console.log("Wszystkie liczny w tablicy: ", numbers);
 for (let i=0; i < numbers.length; i += 1){
 	if (numbers[i] <= treshold){
 	continue;}
-	console.log(`Liczba większa od ${treshold} : ${numbers[i]}`);
+		console.log(`Liczba większa od ${treshold} : ${numbers[i]}`);
 }
